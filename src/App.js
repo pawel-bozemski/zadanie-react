76e3d838ab685dcfb48fee6a866479b2a6504f6c
pawel-bozemski <paw.boz@gmail.com>
2022-01-15 14:43:51 +0100

    Add project to repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState = {
+  click: false,
+  clickCounter: 0,
+};
+
+class App extends React.Component {
+  state = {
+    click: initialState.click,
+    clickCounter: initialState.clickCounter,
+  };
+
+  componentDidUpdate(prevState) {
+    if (prevState.click !== this.state.click) {
+      setTimeout(() => {
+        console.log("state clear");
+        this.setState({ ...initialState });
+      }, 2000);
+    }
+  }
+
+  clickedBox = (e) => {
+    this.setState((state) => ({
+      click: true,
+      clickCounter: ++state.clickCounter,
+    }));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div
+          className={`box 
+          ${this.state.click ? "clicked" : "default"}
+          ${this.state.clickCounter >= 3 ? "multipleClick" : ""}          
+          `}
+          onClick={this.clickedBox}
+        ></div>
+      </div>
+    );
+  }
 }
 
 export default App;
